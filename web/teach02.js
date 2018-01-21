edf3e124817d5ba20b95f5f61586f636ca81e73b
Willans-Jenaca <princesspoofball@yahoo.com>
2018-01-21 01:29:26 -0800

    finished part 1 of teach02

--- web/teach02.js
+++ web/teach02.js
@@ -1,4 +1,14 @@
 function clickme() {
     // This line writes to the console.
-    var answer1 = console.log("Clicked!");
+    alert("Clicked!");
+}
+
+function changeColor() {
+	var textbox_id = "textColor";
+	var textbox = document.getElementById(textbox_id);
+	var div_id = "div1";
+	var div = document.getElementById(div_id);
+
+	var color = textbox.value
+	div.style.backgroundColor = color;
 }
