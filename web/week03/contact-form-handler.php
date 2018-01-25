25d98047f6f5be9883b04d9adf411770eb18560c
Willans-Jenaca <princesspoofball@yahoo.com>
2018-01-24 20:25:11 -0800

    new php rule

--- web/week03/contact-form-handler.php
+++ web/week03/contact-form-handler.php
@@ -8,12 +8,13 @@ if(empty($_POST['name'])  ||
     $errors .= "\n Error: all fields are required";
 }
 
-$name = test_input($_POST["name"]);
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+
 if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
   $errors .= "\n Error: Only letters and white space allowed"; 
 }
 
-$email_address = test_input($_POST["email"]);
 if (!filter_var($email_address, FILTER_VALIDATE_EMAIL)) {
   $errors .= "\n Error: Invalid email address";
 }
