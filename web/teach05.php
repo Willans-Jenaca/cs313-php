774a4fad7c56777e7039e9b99a70cfd5d79cd9ab
Willans-Jenaca <princesspoofball@yahoo.com>
2018-02-05 20:04:25 -0800

    teach php

---
+++ web/teach05.php
@@ -0,0 +1,18 @@
+<?php
+
+$dbUrl = getenv('DATABASE_URL');
+
+echo $dbUrl;
+
+// $dbopts = parse_url($dbUrl);
+
+// $dbHost = $dbopts["host"];
+// $dbPort = $dbopts["port"];
+// $dbUser = $dbopts["user"];
+// $dbPassword = $dbopts["pass"];
+// $dbName = ltrim($dbopts["path"])
+
+// $schemaName = 'scripture_ta';
+// $
+
+?>
