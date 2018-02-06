d900f9b6d4ab8e52e0a1e4a58062aa627914cd80
Willans-Jenaca <princesspoofball@yahoo.com>
2018-02-05 20:23:19 -0800

    full teach.php

--- web/teach05.php
+++ web/teach05.php
@@ -1,18 +1,36 @@
+<html>
+<body>
+
+	<h1>SCRIPTURE RESOURCES</h1>
+
 <?php
 
 $dbUrl = getenv('DATABASE_URL');
+$dbopts = parse_url($dbUrl);
 
-echo $dbUrl;
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"], '/');
 
-// $dbopts = parse_url($dbUrl);
+try {
+ $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+}
+catch (PDOException $ex) {
+ print "<p>error: $ex->getMessage() </p>\n\n";
+ die();
+}
 
-// $dbHost = $dbopts["host"];
-// $dbPort = $dbopts["port"];
-// $dbUser = $dbopts["user"];
-// $dbPassword = $dbopts["pass"];
-// $dbName = ltrim($dbopts["path"])
+$statement = $db->query('SELECT * FROM scriptures');
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-// $schemaName = 'scripture_ta';
-// $
+foreach ($results as $row)
+{
+	echo "<p><strong>$row['book'] $row['chapter']:$row['verse']</strong> - \"$row['content']\"</p><br>";
+}
 
 ?>
+
+</body>
+</html>
