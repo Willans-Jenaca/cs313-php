1dbaad51473773eb2cae026a0c1ff65f8741957f
Willans-Jenaca <princesspoofball@yahoo.com>
2018-02-10 18:16:41 -0800

    first try at acw database display

--- web/week05/prove05.php
+++ web/week05/prove05.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 // Start the Session
 session_start();
 
@@ -7,17 +7,17 @@ session_start();
 $_SESSION["book1"] = htmlspecialchars($_POST["book1"]);
 
 
-?>
+?> -->
 
 
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Jenaca Willans | Cart</title>
+	<title>Jenaca Willans | Database</title>
 	<meta charset="utf-8">
         <meta name="author" content="Jenaca Willans">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="Prove 03 Cart">
+        <meta name="description" content="Prove 05 Database">
         <link href="../normalize.css" rel="stylesheet" 
               media="screen" type="text/css"/>
         <link href="prove05.css" rel="stylesheet" 
@@ -41,56 +41,57 @@ $_SESSION["book1"] = htmlspecialchars($_POST["book1"]);
 			</header> <!--- class headernav ---->
 	<section id="main">
         <h2>Prove 05 Database Access</h2>
-<?php
+            <?php
 
-$dbUrl = getenv('DATABASE_URL');
-$dbopts = parse_url($dbUrl);
+            $dbUrl = getenv('DATABASE_URL');
+            $dbopts = parse_url($dbUrl);
 
-$dbHost = $dbopts["host"];
-$dbPort = $dbopts["port"];
-$dbUser = $dbopts["user"];
-$dbPassword = $dbopts["pass"];
-$dbName = ltrim($dbopts["path"], '/');
+            $dbHost = $dbopts["host"];
+            $dbPort = $dbopts["port"];
+            $dbUser = $dbopts["user"];
+            $dbPassword = $dbopts["pass"];
+            $dbName = ltrim($dbopts["path"], '/');
 
-try {
- $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-}
-catch (PDOException $ex) {
- print "<p>error: $ex->getMessage() </p>\n\n";
- die();
-}
+            try {
+             $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+            }
+            catch (PDOException $ex) {
+             print "<p>error: $ex->getMessage() </p>\n\n";
+             die();
+            }
 
-$statement = $db->query('SELECT * FROM scriptures');
-$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+            $statement = $db->query('SELECT * FROM child');
+            $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-foreach ($results as $row)
-{
-    echo "<p><strong>" . $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . "</strong> - \"" . $row['content'] . "\"</p><br>";
-}
-
-
-$statement = $db->query("SELECT * FROM scriptures WHERE book='" . $_GET['book'] . "'");
-$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($results as $row)
+            {
+                // echo "<p><strong>" . $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . "</strong> - \"" . $row['content'] . "\"</p><br>";
+                echo $row['child_id'] $row['child_first_name'];
+            }
 
 
-?>
+            // $statement = $db->query("SELECT * FROM scriptures WHERE book='" . $_GET['book'] . "'");
+            // $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-<form method="get" name="teach05_form" id="teach05_form"
-      action="teach05.php" >   
-        <label for="name">What book do you want to reference?</label><br>
-        <input type="text" name="book" id="name" size="40" required><br><br>        
-        
-        <input type="submit"><br><br>
 
-        <p>All fields are required.</p><br>
-</form>
+            ?>
 
-<?php
-foreach ($results as $row)
-{
-    echo "<p><strong>" . $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . "</strong> - \"" . $row['content'] . "\"</p><br>";
-}
-?>
+            <form method="get" name="teach05_form" id="teach05_form"
+                  action="teach05.php" >   
+                    <label for="name">What child is this payment for?</label><br>
+                    <input type="text" name="child_first_name" id="name" size="40" required><br><br>        
+                    
+                    <input type="submit"><br><br>
+
+                    <p>All fields are required.</p><br>
+            </form>
+
+            <?php
+            foreach ($results as $row)
+            {
+               echo $row['child_last_name'] $row['child_first_name'];
+            }
+            ?>
 
 	</section>
 		<footer> 
