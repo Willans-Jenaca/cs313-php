db00ee87991a85d442fe7faeb8cc35dc0e3841dc
Willans-Jenaca <princesspoofball@yahoo.com>
2018-02-10 17:56:57 -0800

    first upload of css prove05

---
+++ web/week05/prove05.php
@@ -0,0 +1,101 @@
+<?php
+// Start the Session
+session_start();
+
+// Set CLEAN session variables
+
+$_SESSION["book1"] = htmlspecialchars($_POST["book1"]);
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Jenaca Willans | Cart</title>
+	<meta charset="utf-8">
+        <meta name="author" content="Jenaca Willans">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Prove 03 Cart">
+        <link href="../normalize.css" rel="stylesheet" 
+              media="screen" type="text/css"/>
+        <link href="prove05.css" rel="stylesheet" 
+              media="screen" type="text/css"/>
+</head>
+<body>
+	<div class="wrapper">
+            <header class="headernav">
+            	<nav class="logonav">
+                    <div class="logodiv">
+                        <a href="prove05.php">
+                            <img src="../images/ACWLogo.png" 
+                            alt="A Child's World Logo"
+                            width="518" height="108" class="logo"></a>
+                        <h1 class="hide">PHP Database Access</h1>
+                    </div>  <!---- class logodiv ---->
+                    <ul>
+                        <li><a href="prove05.php">Home</a></li>
+                    </ul> 
+                </nav> <!--- logonav ---->
+			</header> <!--- class headernav ---->
+	<section id="main">
+        <h2>Prove 05 Database Access</h2>
+<?php
+
+$dbUrl = getenv('DATABASE_URL');
+$dbopts = parse_url($dbUrl);
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"], '/');
+
+try {
+ $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+}
+catch (PDOException $ex) {
+ print "<p>error: $ex->getMessage() </p>\n\n";
+ die();
+}
+
+$statement = $db->query('SELECT * FROM scriptures');
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($results as $row)
+{
+    echo "<p><strong>" . $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . "</strong> - \"" . $row['content'] . "\"</p><br>";
+}
+
+
+$statement = $db->query("SELECT * FROM scriptures WHERE book='" . $_GET['book'] . "'");
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+?>
+
+<form method="get" name="teach05_form" id="teach05_form"
+      action="teach05.php" >   
+        <label for="name">What book do you want to reference?</label><br>
+        <input type="text" name="book" id="name" size="40" required><br><br>        
+        
+        <input type="submit"><br><br>
+
+        <p>All fields are required.</p><br>
+</form>
+
+<?php
+foreach ($results as $row)
+{
+    echo "<p><strong>" . $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . "</strong> - \"" . $row['content'] . "\"</p><br>";
+}
+?>
+
+	</section>
+		<footer> 
+            &copy; 2018 | Jenaca Willans
+        </footer>
+    </div> <!--- wrapper ---->
+</body>
+</html>
