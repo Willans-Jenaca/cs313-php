ea193ec51673cdba2c1cbc276ba653803add0aa6
Willans-Jenaca <princesspoofball@yahoo.com>
2018-02-10 21:32:58 -0800

    comment out php

--- web/week05/post.php
+++ web/week05/post.php
@@ -23,64 +23,65 @@ $_SESSION["name"] = htmlspecialchars($_POST["name"]);
 </head>
 <body>
 	<div class="wrapper">
-            <header class="headernav">
-            	<nav class="logonav">
-                    <div class="logodiv">
-                        <a href="prove05.php">
-                            <img src="../images/ACWLogo.png" 
-                            alt="A Child's World Logo"
-                            width="518" height="108" class="logo"></a>
-                        <h1 class="hide">Database Retrieve</h1>
-                    </div>  <!---- class logodiv ---->
-                    <ul>
-                        <li><a href="prove05.php">Home</a></li>
-                    </ul> 
-                </nav> <!--- logonav ---->
+    <header class="headernav">
+    	<nav class="logonav">
+            <div class="logodiv">
+                <a href="prove05.php">
+                    <img src="../images/ACWLogo.png" 
+                    alt="A Child's World Logo"
+                    width="518" height="108" class="logo"></a>
+                <h1 class="hide">Database Retrieve</h1>
+            </div>  <!---- class logodiv ---->
+            <ul>
+                <li><a href="prove05.php">Home</a></li>
+            </ul> 
+        </nav> <!--- logonav ---->
 			</header> <!--- class headernav ---->
-	<section id="main">
-		<h2>Results</h2>
-    <div id="checkoutform">
-      <?php
+    	<section id="main">
+    		<h2>Results</h2>
+        <div id="phpdiv">
+          <?php
 
-            $dbUrl = getenv('DATABASE_URL');
-            $dbopts = parse_url($dbUrl);
+                // $dbUrl = getenv('DATABASE_URL');
+                // $dbopts = parse_url($dbUrl);
 
-            $dbHost = $dbopts["host"];
-            $dbPort = $dbopts["port"];
-            $dbUser = $dbopts["user"];
-            $dbPassword = $dbopts["pass"];
-            $dbName = ltrim($dbopts["path"], '/');
+                // $dbHost = $dbopts["host"];
+                // $dbPort = $dbopts["port"];
+                // $dbUser = $dbopts["user"];
+                // $dbPassword = $dbopts["pass"];
+                // $dbName = ltrim($dbopts["path"], '/');
 
-            try {
-             $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-            }
-            catch (PDOException $ex) {
-             print "<p>error: $ex->getMessage() </p>\n\n";
-             die();
-            }
+                // try {
+                //  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+                // }
+                // catch (PDOException $ex) {
+                //  print "<p>error: $ex->getMessage() </p>\n\n";
+                //  die();
+                // }
 
             
-  $statement = $db->query("SELECT * FROM acw.child WHERE child_first_name='" . $_SESSION['name'] . "'");
-  $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+  // $statement = $db->query("SELECT * FROM acw.child WHERE child_first_name='" . $_SESSION['name'] . "'");
+  // $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-          foreach ($results as $row)
-          {
-              echo "<p>" . $row['child_last_name'] . ", " . $row['child_first_name'] "</p>";
-          }
-          ?> 
-          
-        <!--   <br><button onclick="goBack()">Back Search</button>
-          <script>
-              function goBack() {
-                window.history.back();
-              }
-          </script> -->
-          <br>
-      </div> <!--- id acwpaymentform ---->   
-	</section>
+  //               foreach ($results as $row)
+  //               {
+  //                  echo "<p>" . $row['child_last_name'] . "</p>";
+  //                   // echo "<p>" . $row['child_last_name'] . ", " . $row['child_first_name'] "</p>";
+  //               }
+                ?> 
+                
+              <!--   <br><button onclick="goBack()">Back Search</button>
+                <script>
+                    function goBack() {
+                      window.history.back();
+                    }
+                </script> -->
+                <br>
+            </div> <!--- id phpdiv ---->   
+      </section>
 		<footer> 
             &copy; 2018 | Jenaca Willans
-        </footer>
-    </div> <!--- wrapper ---->
+    </footer>
+  </div> <!--- wrapper ---->
 </body>
 </html>
