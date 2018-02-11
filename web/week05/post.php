29f4045bca081d1285ecd4f494f3c29ee7e55eb6
Willans-Jenaca <princesspoofball@yahoo.com>
2018-02-10 21:34:38 -0800

    adding in piece one

--- web/week05/post.php
+++ web/week05/post.php
@@ -42,22 +42,22 @@ $_SESSION["name"] = htmlspecialchars($_POST["name"]);
         <div id="phpdiv">
           <?php
 
-                // $dbUrl = getenv('DATABASE_URL');
-                // $dbopts = parse_url($dbUrl);
+                $dbUrl = getenv('DATABASE_URL');
+                $dbopts = parse_url($dbUrl);
 
-                // $dbHost = $dbopts["host"];
-                // $dbPort = $dbopts["port"];
-                // $dbUser = $dbopts["user"];
-                // $dbPassword = $dbopts["pass"];
-                // $dbName = ltrim($dbopts["path"], '/');
+                $dbHost = $dbopts["host"];
+                $dbPort = $dbopts["port"];
+                $dbUser = $dbopts["user"];
+                $dbPassword = $dbopts["pass"];
+                $dbName = ltrim($dbopts["path"], '/');
 
-                // try {
-                //  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-                // }
-                // catch (PDOException $ex) {
-                //  print "<p>error: $ex->getMessage() </p>\n\n";
-                //  die();
-                // }
+                try {
+                 $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+                }
+                catch (PDOException $ex) {
+                 print "<p>error: $ex->getMessage() </p>\n\n";
+                 die();
+                }
 
             
   // $statement = $db->query("SELECT * FROM acw.child WHERE child_first_name='" . $_SESSION['name'] . "'");
